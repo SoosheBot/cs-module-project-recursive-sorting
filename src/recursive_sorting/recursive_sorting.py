d01fd4823feece9f654d4f0da210e4d88a5fb3e9
sooshe <sooshe@sooshebot.com>
2020-06-08 21:00:22 -0500

    wrote an initial merge function

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,7 +4,24 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
+    # To-do
+    # Loop through the merged array
+    # If the arrays are bigger than zero, find out which element is bigger
+    # pop(?) that smaller element into the merged_arr
+    for i in range(merged_arr):
+        if len(arrA) > 0 and len(arrB) > 0:
+            #if arrA at the 0th position is bigger than arrB at the 0th position, pop out the element in arrB's 0th position
+            if arrA[0] > arrB[0]:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                #pop the element in arrA's 0th position
+                merged_arr[i] = arrA.pop(0)
+        else:
+            # if arrA is empty, add rest of arrB to merged array
+            if len(arrA) == 0:
+                merged_arr[i] = arrB.pop(0)
+            else:
+                merged_arr[i] = arrA.pop(0)
 
     return merged_arr
 
